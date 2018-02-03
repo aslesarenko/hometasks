c5a02d690f898048ff2e602a28f1ace2e13be867
Alexander Slesarenko <avslesarenko@gmail.com>
2018-02-03 17:38:31 +0300

    solution using simple loop with scala.util.Random

--- src/main/scala/mining/PoWMiner.scala
+++ src/main/scala/mining/PoWMiner.scala
@@ -3,12 +3,21 @@ package mining
 import scorex.crypto.hash.CryptographicHash32
 
 import scala.math.BigInt
+import scala.util.Random
 
 class PoWMiner[HF <: CryptographicHash32](hashFunction: HF) {
 
   private val MaxTarget: BigInt = BigInt(1, Array.fill(32)((-1).toByte))
 
-  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = ???
+  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = {
+    val rnd = new Random()
+    var proved: ProvedData = null
+    do {
+      val nonce = rnd.nextInt()
+      proved = ProvedData(data, nonce)
+    } while (!validateWork(proved, difficulty))
+    proved
+  }
 
   def validateWork(data: ProvedData, difficulty: BigInt): Boolean = realDifficulty(data) >= difficulty
 
--- src/test/scala/mining/PoWMinerTest.scala
+++ src/test/scala/mining/PoWMinerTest.scala
@@ -1,7 +1,8 @@
 package mining
 
-import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
-import org.scalatest.{Matchers, PropSpec}
+import org.scalacheck.Gen
+import org.scalatest.prop.{PropertyChecks, GeneratorDrivenPropertyChecks}
+import org.scalatest.{PropSpec, Matchers}
 import scorex.crypto.hash.Blake2b256
 import scorex.testkit.generators.CoreGenerators
 
@@ -13,7 +14,8 @@ class PoWMinerTest extends PropSpec
 
   property("should generate valid prof for S") {
     val miner = new PoWMiner(Blake2b256)
-    forAll(smallInt, nonEmptyBytesGen) { (difficulty, data) =>
+    lazy val diffGen: Gen[Int] = Gen.choose(0, 1000000)
+    forAll(diffGen, nonEmptyBytesGen) { (difficulty, data) =>
       val proved = miner.doWork(data, difficulty)
       proved.data shouldEqual data
       miner.validateWork(proved, difficulty) shouldBe true
